867ac49308bd6b4c97546aa2c9684ae565744951
Abdul Muthallib <abdulmuthallib984@gmail.com>
2024-02-02 04:09:51 +0530

    kadanes algorithm

---
+++ 53. Maximum Subarray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int num: nums){
+            sum = sum + num;                               /// kadanes algorithm
+           max = Math.max(sum,max);
+            if(sum<0) sum = 0;
+        }
+        return max;
+    }
+}
