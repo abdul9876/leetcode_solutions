03f5a8e2287fee42abe426620cb891f8405788b3
Abdul Muthallib <abdulmuthallib984@gmail.com>
2025-06-04 01:13:43 +0530

    random pointer and a normal pointer

---
+++ 138. Copy List with Random Pointer.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        Node temp = head;
+        Map<Node, Node> mymap = new HashMap<>();
+        while(temp!=null){
+         Node newnode = new Node(temp.val);
+         mymap.put(temp,newnode);
+         temp = temp.next;
+        }
+        temp = head;
+        while(temp!=null){
+            Node copynode = mymap.get(temp);
+            copynode.next = mymap.get(temp.next);
+            copynode.random = mymap.get(temp.random);
+            temp = temp.next;
+        }
+        return mymap.get(head);
+    }
+}
