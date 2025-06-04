738abf448797899b0a7a17aada06c7127484d3fd
Abdul Muthallib <abdulmuthallib984@gmail.com>
2025-06-05 01:50:21 +0530

    random pointer.java

--- 138. Copy List with Random Pointer.java
+++ 138. Copy List with Random Pointer.java
@@ -32,3 +32,64 @@ class Solution {
         return mymap.get(head);
     }
 }
+                          // Optimal solution where the space complexity is reduced to O(n) 
+                         // time complexity is O(3n)
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node crcopy(Node head){
+        Node temp = head;
+        while(temp!=null){
+         Node copynode = new Node(temp.val);
+         copynode.next = temp.next;
+         temp.next = copynode;
+         temp = temp.next.next;
+        }
+        return head;a
+    }
+    public Node rand(Node head){
+        Node temp = head;
+        while(temp!=null){
+            Node copynode = temp.next;
+            if(temp.random!=null){
+                copynode.random = temp.random.next;
+            }
+            else{
+                copynode.random = null;
+            }
+            temp = temp.next.next;
+        }
+        return head;
+    }
+    public Node rearrange(Node head){
+        Node temp = head;
+      Node dummy = new Node(-1);
+      Node res = dummy;
+      while(temp!=null){
+        res.next = temp.next;
+        temp.next = temp.next.next;
+        res = res.next;
+        temp = temp.next;
+      }
+      return dummy.next;
+    }
+    public Node copyRandomList(Node head) {
+      crcopy(head);
+     rand(head);
+    return rearrange(head);
+    
+}
+}
